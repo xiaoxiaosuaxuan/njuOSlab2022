9a61dc7cfe3200b94980434463f3a2e1b95000b6
tracer-nju <tracer@nju.edu.cn>
2022-07-16 02:00:33 -0700

    Linux ubuntu 5.4.0-122-generic #138~18.04.1-Ubuntu SMP Fri Jun 24 14:14:03 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:00:33 up  4:34,  4 users,  load average: 0.05, 0.06, 0.01

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -157,9 +157,8 @@ int printNode(int i){                //打印进程名(PID)，返回打印的长
 
 
 void printTree(int index, int offset, int newline, int len, int* arr){    //递归打印出树
-	assert(pArray[index]->pid != 1471);
 	if (pArray[index]->pid == 1471){
-		printf("i'm 1471 aaaaaa\n");
+		printf("i'm 1471 aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n");
 		struct pnode* a = pArray[index];
 		if (a->nxtBrother != -1){
 			printf("%s(%d)aaaaaaa", pArray[a->nxtBrother]->name, pArray[a->nxtBrother]->pid);
