7eeae1106f98161f54ec6b757d2c05521cbb67f1
tracer-nju <tracer@nju.edu.cn>
2022-07-16 01:25:42 -0700

    Linux ubuntu 5.4.0-122-generic #138~18.04.1-Ubuntu SMP Fri Jun 24 14:14:03 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:25:42 up  3:59,  4 users,  load average: 0.01, 0.01, 0.00

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -209,7 +209,7 @@ void printTree(int index, int offset, int newline, int len, int* arr){    //递�
 			tmp = pArray[tmp->nxtBrother];
 		}
 	}
-	if (index != 0 && pArray[index]->nxtBrother == -1) {free(arr); printf("aaaaaaaa\n");}      //没有下一个兄弟，说明是最后使用arr的，由它来释放空间
+	if (index != 0 && pArray[index]->nxtBrother == -1) {free(arr); printf("\naaaaaaaa");}      //没有下一个兄弟，说明是最后使用arr的，由它来释放空间
 
 }
 
